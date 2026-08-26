b7da375cf7d096167e674c9b549fcfa15dc7236e
Mikhail Vospennikov <157801994+mikhailvospennikov@users.noreply.github.com>
2026-08-26 17:40:50 +0300

    Update ios to 2.1.0 (#34)

--- ios/zing_sdk_initializer/Package.swift
+++ ios/zing_sdk_initializer/Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "zing-sdk-initializer", targets: ["zing_sdk_initializer"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Muze-Fitness/zing-coach-sdk-ios", exact: "2.0.0")
+        .package(url: "https://github.com/Muze-Fitness/zing-coach-sdk-ios", exact: "2.1.0")
     ],
     targets: [
         .target(
